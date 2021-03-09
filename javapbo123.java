402513851b96f16c16e3227782770d990d21c449
vigorjs <80097275+vigorjs@users.noreply.github.com>
2021-03-09 14:18:20 +0700

    Add files via upload
    Tugas PBO

---
+++ javapbo123.java
@@ -0,0 +1,11 @@
+//Virgo Fajar Pamungkas - A11.2019.12289
+package java123;
+
+public class javapbo123 {
+	public static void main(String[] args) {
+		// mainclass "javpbo123"
+		System.out.println("latian java pbo 1");
+		int a,b,c,d;
+		System.out.println((a = -5+8*6) + "\n" + (b = (55+9)%9) + "\n" + (c = 20+-3*5/8) + "\n" + (d = 5+15/3*2-8%3));        
+	}
+}
